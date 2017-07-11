9919d05cb680ac5abda273f674a5e5136e9520f1
Chaofan Zhang <chaofan.zhang@ef.com>
2017-07-11 16:35:33 +0800

    Create the shooting bolt, with Quad, material, texture, add rigidbody, capsule collider, and mover script.

---
+++ Assets/Scripts/BoltMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltMover : MonoBehaviour {
+
+	public float speed = 20.0f;
+
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+
+}
