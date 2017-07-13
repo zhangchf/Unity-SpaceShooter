97ddc5d071197ff005797ef0f6a844c4445dbd1e
Chaofan Zhang <chaofan.zhang@ef.com>
2017-07-13 12:25:44 +0800

    1. Add StarField particle system as moving star bg. 2. Moving the Background by repeating two connecting BG images.

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	public float scrollSpeed;
+
+	Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float movement = Mathf.Repeat (Time.time * scrollSpeed, transform.localScale.y);
+		Debug.Log ("movement=" + movement);
+		transform.position = startPosition + Vector3.forward * movement;
+		
+	}
+}
