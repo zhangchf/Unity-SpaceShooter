8bdb8b60a468bc33b4b0f73bc45a8cf553eac700
Chaofan Zhang <chaofan.zhang@ef.com>
2017-07-13 16:57:45 +0800

    Add enemy evasion movement to add some AI flavor.

---
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour {
+
+	public Vector2 startWait;
+	public Vector2 maneuverTime;
+	public Vector2 maneuverWait;
+	public float dodge;
+	public float smoothing;
+	public float tilt;
+	public Boundary boundary;
+
+	Rigidbody rb;
+	float targetManeuver;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		StartCoroutine (Dodge ());
+	}
+
+	IEnumerator Dodge() {
+		yield return new WaitForSeconds (Random.Range (startWait.x, startWait.y));
+
+		while(true) {
+			targetManeuver = Random.Range (-dodge, dodge);
+			yield return new WaitForSeconds (Random.Range (maneuverTime.x, maneuverTime.y));
+			targetManeuver = 0;
+			yield return new WaitForSeconds (Random.Range (maneuverWait.x, maneuverWait.y));
+		}
+	}
+
+	void FixedUpdate() {
+		float velocityX = Mathf.MoveTowards (rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+		rb.velocity = new Vector3 (velocityX, 0f, rb.velocity.z);
+
+		float clampedX = Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax);
+		float clampedZ = Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax);
+		rb.position = new Vector3 (clampedX, 0.0f, clampedZ);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+
+
+}
